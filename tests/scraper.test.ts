106673524ffe00b12bce93033186ad29ff82f96f
Wilmer Azevedo <wddesousa@gmail.com>
2024-12-19 22:32:47 +0000

    t

--- tests/scraper.test.ts
+++ tests/scraper.test.ts
@@ -3,4 +3,5 @@ import { Scrape } from '@/app/api/scraper/utils'
 
 test('scraper', async () => {
     const test = await Scrape('https://pcpartpicker.com/product/3hyH99/amd-ryzen-7-7800x3d-42-ghz-8-core-processor-100-100000910wof')
+    console.log(test)
 })
