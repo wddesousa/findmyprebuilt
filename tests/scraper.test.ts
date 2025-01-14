ac649131335a745b533b6a66613d202b07e51742
Wilmer <wddesousa@gmail.com>
2025-01-14 15:56:46 +0000

    improved readability

--- tests/scraper.test.ts
+++ tests/scraper.test.ts
@@ -1,408 +1,76 @@
-import {describe, expect, test} from 'vitest'
-import { scrapeAndSavePart } from '@/app/api/scrapers/utils'
-import { Prisma, PrismaClient } from '@prisma/client'
-import { extractUsbNumbers } from '@/app/api/scrapers/mobachipsets/utils'
-import { mobaChipsetCustomSerializer } from '@/app/api/scrapers/serializers'
-import path from 'path'
-import { pathToFileURL } from 'url'
-import { PrismaModelMap } from '@/app/api/scrapers/types'
+import { describe, expect, test } from "vitest";
+import { scrapeAndSavePart } from "@/app/api/scrapers/utils";
+import { Prisma, PrismaClient } from "@prisma/client";
+import { extractUsbNumbers } from "@/app/api/scrapers/mobachipsets/utils";
+import { mobaChipsetCustomSerializer } from "@/app/api/scrapers/serializers";
+import path from "path";
+import { pathToFileURL } from "url";
+import { PrismaModelMap } from "@/app/api/scrapers/types";
+import { psuResult, airCoolerResult, caseFanResult, caseResult, cpuResult, gpuResult, hddStorageResult, liquidCoolerResult, memoryResult, mobaResult, ssdStorageResult } from "./results";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
-test('correctly extracts usb number', () => {
-  var string = '\n                                                        \n                                                            \n                                                            \n                                                                10 USB 3.2 Ports- Up to 4 USB 3.2 Gen 2x2 (20Gb/s) Ports- Up to 10 USB 3.2 Gen 2x1 (10Gb/s) Ports- Up to 2 USB 3.2 Gen 1x1 (5Gb/s) Ports14 USB 2.0 Ports\n                                                            \n                                                        \n                                                    '
-  expect(extractUsbNumbers(string, '20', 'speed')).toEqual(4)
-  expect(extractUsbNumbers(string, '10', 'speed')).toEqual(10)
-  expect(extractUsbNumbers(string, '5', 'speed')).toEqual(2)
-  expect(extractUsbNumbers(string, '342', 'speed')).toEqual(0)
-  
-  string = 
-  '\n                                                        \n                                                            \n                                                            \n                                                                10 USB 3.0 Ports14 USB 2.0 Ports\n                                                            \n                                                        \n                                                    '
-  expect(extractUsbNumbers(string, '3.0', 'version')).toEqual(10)
-  expect(extractUsbNumbers(string, '2.0', 'version')).toEqual(14)
+test("correctly extracts usb number", () => {
+  var string =
+    "\n                                                        \n                                                            \n                                                            \n                                                                10 USB 3.2 Ports- Up to 4 USB 3.2 Gen 2x2 (20Gb/s) Ports- Up to 10 USB 3.2 Gen 2x1 (10Gb/s) Ports- Up to 2 USB 3.2 Gen 1x1 (5Gb/s) Ports14 USB 2.0 Ports\n                                                            \n                                                        \n                                                    ";
+  expect(extractUsbNumbers(string, "20", "speed")).toEqual(4);
+  expect(extractUsbNumbers(string, "10", "speed")).toEqual(10);
+  expect(extractUsbNumbers(string, "5", "speed")).toEqual(2);
+  expect(extractUsbNumbers(string, "342", "speed")).toEqual(0);
 
-})
+  string =
+    "\n                                                        \n                                                            \n                                                            \n                                                                10 USB 3.0 Ports14 USB 2.0 Ports\n                                                            \n                                                        \n                                                    ";
+  expect(extractUsbNumbers(string, "3.0", "version")).toEqual(10);
+  expect(extractUsbNumbers(string, "2.0", "version")).toEqual(14);
+});
 
-test('correcly extract pci generation', () => {
-  expect(mobaChipsetCustomSerializer['intel']['pci_generation']!('\n                                                        \n                                                            \n                                                            \n                                                                3.0, 4.0\n                                                            \n                                                        \n                                                    ')).toBe(4)
-  
-  expect(mobaChipsetCustomSerializer['intel']['pci_generation']!('\n                                                        \n                                                            \n                                                            \n                                                                3.0\n                                                            \n                                                        \n                                                    ')).toBe(3)
-})
-describe('parts specs scraper', async () => {
-  const getFile = (filename: string) => pathToFileURL(path.join(__dirname, './data', filename)).href
+test("correcly extract pci generation", () => {
+  expect(
+    mobaChipsetCustomSerializer["intel"]["pci_generation"]!(
+      "\n                                                        \n                                                            \n                                                            \n                                                                3.0, 4.0\n                                                            \n                                                        \n                                                    "
+    )
+  ).toBe(4);
 
-    try {
-        await prisma.product.deleteMany({ where: {
-            url: {
-              startsWith: 'file://'
-            }
-        } })
-    } catch (error: any) {
-      if (error.code !== 'P2025') {
-        console.error(error)
-        process.exit(error.code)
-      }
+  expect(
+    mobaChipsetCustomSerializer["intel"]["pci_generation"]!(
+      "\n                                                        \n                                                            \n                                                            \n                                                                3.0\n                                                            \n                                                        \n                                                    "
+    )
+  ).toBe(3);
+});
+describe("parts specs scraper", async () => {
+  const getFile = (filename: string) =>
+    pathToFileURL(path.join(__dirname, "./data", filename)).href;
+
+  try {
+    await prisma.product.deleteMany({
+      where: {
+        url: {
+          startsWith: "file://",
+        },
+      },
+    });
+  } catch (error: any) {
+    if (error.code !== "P2025") {
+      console.error(error);
+      process.exit(error.code);
     }
+  }
 
-
-
-    test('caseFan', async () => {
-      const file = getFile("case_fan.html")
-      console.log(file)
-      const caseFan = await scrapeAndSavePart(file)
-      expect(caseFan).toMatchObject({
-        part_number: [ 'UF-SLIN120-3B', '12SLIN3B' ],
-        size_mm: 120,
-        color: 'Black',
-        quantity: '3-Pack',
-        airflow: '0 - 61.3 CFM',
-        noise_level: '0 - 29 dB',
-        pwm: true,
-        led: 'Addressable RGB',
-        connector: '4-pin PWM + 3-pin 5V Addressable RGB',
-        controller: '5V Addressable RGB',
-        static_pressure_mmh2o: new Prisma.Decimal(2.66),
-        product: {
-          name: 'Uni Fan SL-Infinity',
-          type: 'CASEFAN',
-          asin: null,
-          url: file,
-          brand: { name: 'Lian Li' }
-        }
-      })
-  })
-
-    test('case', async () => {
-      const file = getFile("case.html")
-      const pc_case = await scrapeAndSavePart(file)
-      expect(pc_case).toMatchObject({
-        part_number: [ 'CC-H61FB-01' ],
-        type: 'ATX Mid Tower',
-        color: 'Black',
-        power_supply: false,
-        side_panel: 'Tempered Glass',
-        power_supply_shroud: true,
-        front_panel_usb: [ 'USB 3.2 Gen 2 Type-C', 'USB 3.2 Gen 1 Type-A' ],
-        maximum_video_card_length_mm: 365,
-        drive_bays: [ '2 x Internal 2.5"', '1 x Internal 3.5"' ],
-        expansion_slots: '7 x Full-Height',
-        volume_ml: 51811,
-        dimensions: [ '415 mm x 287 mm x 435 mm', '16.339" x 11.299" x 17.126"' ],
-        product: {
-          name: 'H6 Flow',
-          type: 'CASE',
-          url: file,
-          asin: null,
-          brand: { name: 'NZXT' }
-        },
-        moba_form_factors: [
-          { name: 'ATX' },
-          { name: 'Micro ATX' },
-          { name: 'Mini ITX' }
-        ]
-      })
-  })
-
-    test('psu', async () => {
-      const file = getFile("psu.html")
-      const psu = await scrapeAndSavePart(file)
-      expect(psu).toMatchObject({
-        part_number: [
-          'CP-9020262-NA',
-          'CP-9020262-UK',
-          'CP-9020262-EU',
-          'CP-9020262-AU'
-        ],
-        type: 'ATX',
-        efficiency_rating: '80+ Gold',
-        wattage_w: 750,
-        length_mm: 140,
-        modular: 'Full',
-        color: 'Black',
-        fanless: false,
-        atx_4_pin_connectors: 0,
-        eps_8_pin_connectors: 2,
-        pcie_12_4_pin_12vhpwr_connectors: 1,
-        pcie_12_pin_connectors: 0,
-        pcie_8_pin_connectors: 0,
-        pcie_6_2_pin_connectors: 3,
-        pcie_6_pin_connectors: 0,
-        sata_connectors: 7,
-        molex_4_pin_connectors: 4,
-        product: {
-          name: 'RM750e (2023)',
-          type: 'PSU',
-          url: file,
-          asin: null,
-          brand: { name: 'Corsair' }
-        }
-      })
-  })
-
-    test('cooler', async () => {
-      const tests: Record<string, Record<string, any>> = {
-        cooler: {
-          part_number: [ 'ACFRE00124A' ],
-          fan_rpm: '200 - 2000 RPM',
-          color: 'Black',
-          height_mm: 159,
-          water_cooled_radiador_mm: null,
-          fanless: false,
-          product: {
-            name: 'Freezer 36 A-RGB',
-            type: 'COOLER',
-            url: getFile('cooler.html'),
-            asin: null,
-            brand: { name: 'ARCTIC' }
-          },
-          cpu_sockets: [
-            { name: 'AM4' },
-            { name: 'AM5' },
-            { name: 'LGA1700' },
-            { name: 'LGA1851' }
-          ]
-        },
-        liquid_cooler: {
-          part_number: [ 'CW-9060073-WW' ],
-          fan_rpm: '550 - 2100 RPM',
-          color: 'White',
-          height_mm: null,
-          water_cooled_radiador_mm: 360,
-          fanless: false,
-          product: {
-            name: 'iCUE H150i ELITE CAPELLIX XT',
-            type: 'COOLER',
-            url: getFile('liquid_cooler.html'),
-            asin: null,
-            brand: { name: 'Corsair' }
-          },
-          cpu_sockets: [
-            { name: 'AM4' },
-            { name: 'AM5' },
-            { name: 'LGA1150' },
-            { name: 'LGA1151' },
-            { name: 'LGA1155' },
-            { name: 'LGA1156' },
-            { name: 'LGA1200' },
-            { name: 'LGA1700' },
-            { name: 'LGA1851' },
-            { name: 'LGA2011' },
-            { name: 'LGA2011-3' },
-            { name: 'LGA2066' },
-            { name: 'sTR4' },
-            { name: 'sTRX4' }
-          ]
-        }
-      }
-      const promises = Object.keys(tests).map(async (file) => {
-
-        const cooler = await scrapeAndSavePart(getFile(`${file}.html`)) as unknown as PrismaModelMap['cooler']
-        console.log(cooler)
-        expect(cooler).toMatchObject(tests[file])  
-      })
-      await Promise.all(promises)
-    })
-    
-    test('storage', async () => {
-      const tests: Record<string, Record<string, any>> = {
-        storage_hdd: {
-          part_number: [ 'ST24000NT002' ],
-          capacity_gb: 24000,
-          form_factor: '3.5"',
-          interface: 'SATA 6.0 Gb/s',
-          nvme: false,
-          product: {
-            name: 'IronWolf Pro 24 TB',
-            type: 'STORAGE',
-            asin: null,
-            brand: { name: 'Seagate' }
-          },
-          storage_type: { name: '7200 RPM' }
-        },
-        storage_ssd: {
-          part_number: [ 'SKC3000S/1024G' ],
-          capacity_gb: 1024,
-          form_factor: 'M.2-2280',
-          interface: 'M.2 PCIe 4.0 X4',
-          nvme: true,
-          product: {
-            name: 'KC3000 1.024 TB',
-            type: 'STORAGE',
-            asin: null,
-            brand: { name: 'Kingston' }
-          },
-          storage_type: { name: 'SSD' }
-        }
-      }
-      const promises = Object.keys(tests).map(async (file) => {
-
-        const storage = await scrapeAndSavePart(getFile(`${file}.html`)) as unknown as PrismaModelMap['storage']
-        // console.log(storage)
-        expect(storage).toMatchObject(tests[file])  
-      })
-      await Promise.all(promises)
-    })
-
-    test('cpu', async () => {
-      const file = getFile("cpu.html")
-      const cpu = await scrapeAndSavePart(file)
-    expect(cpu).toMatchObject({
-        part_number: ['100-100000910WOF'],
-        series: 'AMD Ryzen 7',
-        microarchitecture: 'Zen 4',
-        core_family: 'Raphael',
-        core_count: 8,
-        thread_count: 16,
-        performance_core_clock_ghz: 4.2,
-        performance_core_boost_clock_ghz: 5,
-        l2_cache_mb: 8,
-        l3_cache_mb: 96,
-        tdp_w: 120,
-        integrated_graphics: 'Radeon',
-        maximum_supported_memory_gb: 128,
-        ecc_support: true,
-        includes_cooler: false,
-        packaging: 'Boxed',
-        lithography_nm: 5,
-        includes_cpu_cooler: false,
-        simultaneous_multithreading: true,
-        product: {
-          name: 'Ryzen 7 7800X3D',
-          type: 'CPU',
-          url: file,
-          brand: { name: 'AMD' }
-        },
-        socket: {
-          name: 'AM5',
-        }
-      })
-    })
-
-    test('gpu', async () => {
-        const file = getFile("gpu.html")
-        const gpu = await scrapeAndSavePart(file)
-        expect(gpu).toMatchObject({
-           boost_clock_mhz: 1777,
-           case_expansion_slot_width: 2,
-           chipset: {
-            name: "GeForce RTX 3060 12GB",
-           },
-           color: "Black",
-           cooling: 2,
-           core_clock_mhz: 1320,
-           displayport_outputs: 3,
-           effective_memory_clock_mhz: 15000,
-           external_power: "1 PCIe 8-pin",
-           frame_sync: "G-Sync",
-           hdmi_outputs: 1,
-           interface: "PCIe x16",
-           length_mm: 235,
-           memory_gb: 12,
-           memory_type: "GDDR6",
-           part_number: [
-             "RTX3060Ventus2X12GOC",
-             "GeForce RTX 3060 VENTUS 2X 12G OC",
-             "V397-022R",
-             "912-V397-039",
-           ],
-           product: {
-             name: "GeForce RTX 3060 Ventus 2X 12G",
-             type: "GPU",
-             url: file,
-             brand: { name: 'MSI' }
-           },
-           tdp_w: 170,
-           total_slot_width: 2,
-        })
-    })
-    
-    test('moba', async () => {
-      const file = getFile("moba.html")
-      const moba = await scrapeAndSavePart(file) as unknown as PrismaModelMap['moba']
-      expect(moba).toMatchObject(
-        {
-          part_number: [ 'Z890 Steel Legend WiFi', '90-MXBPF0-A0UAYZ' ],
-          memory_max: 256,
-          memory_slots: 4,
-          color: 'Silver',
-          pcie_x16_slots: 2,
-          pcie_x8_slots: 0,
-          pcie_x_slots: 1,
-          pcie_x1_slots: 0,
-          pci_slots: 0,
-          mini_pcie_slots: 0,
-          half_mini_pcie_slots: 0,
-          mini_pcie_msata_slots: 0,
-          msata_slots: 0,
-          sata_6_0_gbs: 4,
-          onboard_ethernet: '1 x 2.5 Gb/s (Realtek Dragon RTL8125BG)',
-          onboard_video: 'Depends on CPU',
-          usb_2_0_headers: 2,
-          usb_2_0_headers_single_port: 0,
-          usb_3_2_gen_1_headers: 2,
-          usb_3_2_gen_2_headers: 0,
-          usb_3_2_gen_2x2_headers: 1,
-          supports_ecc: false,
-          wireless_networking: 'Wi-Fi 7',
-          raid_support: true,
-          uses_back_connect_connectors: false,
-          product: {
-            name: 'Z890 Steel Legend WiFi',
-            type: 'MOBA',
-            url: file,
-            asin: null,
-            brand: { name: 'ASRock' }
-          },
-          chipset: {      
-            name: 'Z890',
-          },
-          memory_speeds: [
-            { ddr: 'DDR5', speed: 4400 },
-            { ddr: 'DDR5', speed: 4800 },
-            { ddr: 'DDR5', speed: 5200 },
-            { ddr: 'DDR5', speed: 5600 },
-            { ddr: 'DDR5', speed: 6000 },
-            { ddr: 'DDR5', speed: 6200 },
-            { ddr: 'DDR5', speed: 6400 },
-            { ddr: 'DDR5', speed: 6600 },
-            { ddr: 'DDR5', speed: 6800 },
-            { ddr: 'DDR5', speed: 7000 },
-            { ddr: 'DDR5', speed: 7200 },
-            { ddr: 'DDR5', speed: 7600 },
-            { ddr: 'DDR5', speed: 7800 },
-            { ddr: 'DDR5', speed: 8000 },
-            { ddr: 'DDR5', speed: 8200 },
-            { ddr: 'DDR5', speed: 8400 }
-          ],
-          moba_form_factor: { name: 'ATX' },
-          socket: { name: 'LGA1851' }
-        }
-      )
-      expect(moba['m_2_slots']).toHaveLength(5)
-    })
-
-    test('memory', async () => {
-      const file = getFile("memory.html")
-      const memory = await scrapeAndSavePart(file) as unknown as PrismaModelMap['memory']
-      expect(memory).toMatchObject({
-        part_number: [ 'CMK16GX4M2B3200C16' ],
-        form_factor: '288-pin DIMM (DDR4)',
-        modules: '2 x 8GB',
-        color: 'Black / Yellow',
-        first_word_latency: 10,
-        cas_latency: 16,
-        voltage: new Prisma.Decimal(1.35),
-        timing: '16-18-18-36',
-        ecc_registered: 'Non-ECC / Unbuffered',
-        heat_spreader: true,
-        product: {
-          name: 'Vengeance LPX 16 GB',
-          type: 'MEMORY',
-          url:  file,
-          asin: null,
-          brand: { name: 'Corsair' }
-        },
-        memory_speed: { ddr: 'DDR4', speed: 3200 }
-      })
-    })
-})
+  test.each([
+    [ "case_fan", caseFanResult ],
+    [ "case", caseResult ],
+    [ "psu", psuResult    ],
+    [ "cooler" , airCoolerResult],
+    [ "liquid_cooler", liquidCoolerResult ],
+    [ "storage_hdd", hddStorageResult ],
+    [ "storage_ssd", ssdStorageResult ],
+    [ "cpu", cpuResult ],
+    [ "gpu", gpuResult ],
+    [ "moba", mobaResult ],
+    [ "memory", memoryResult ],
+    ])("%s", async (fileName, expected) => {
+    const file = getFile(`${fileName}.html`);
+    const part = await scrapeAndSavePart(file);
+    expect(part).toMatchObject(expected);
+  });
+});
