18a78ec363641e3f1c5bbcea583af0ab1ff60483
Wilmer <wddesousa@gmail.com>
2025-03-06 16:40:21 +0000

    images and store links

--- app/admin/create/prebuilt/components/form.tsx
+++ app/admin/create/prebuilt/components/form.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useActionState, useEffect, useState } from "react";
+import React, { useActionState, useEffect, useRef, useState } from "react";
 import { submitPrebuilt } from "../action";
 import {
   cleanedResults,
@@ -11,6 +11,7 @@ import { useDebouncedCallback } from "use-debounce";
 import { prebuiltForeignValues } from "../types";
 import { productSearchResult } from "@/app/types";
 import { fetchPrebuilt, inputMap, searchValue } from "../utils/client";
+import { v4 as uuidv4 } from 'uuid';
 
 const CheckboxInput = ({
   label,
@@ -211,14 +212,14 @@ export const SearchInput = ({
   );
 };
 
-export const  ProductNameInput = ({
+export const ProductNameInput = ({
   brandName,
   prebuiltName,
 }: {
   brandName: string;
   prebuiltName: string;
 }) => {
-  const fullName = `${brandName} ${prebuiltName}`
+  const fullName = `${brandName} ${prebuiltName}`;
   const [productName, setProductName] = useState<string>(fullName);
   const [prebuiltExists, setPrebuiltExists] = useState<boolean>(false);
 
@@ -232,11 +233,19 @@ export const  ProductNameInput = ({
     }
   };
 
+  //check that the found name doesn't exist
+  useEffect(() => {
+    fetchPrebuilt(productName)
+      .then((result) => setPrebuiltExists(result.length > 0))
+      .catch((error) => console.error("error fetching productName", error));
+  }, []);
+
   useEffect(() => {
     if (prebuiltExists) {
       alert("Product already exists!");
     }
   }, [prebuiltExists]);
+
   return (
     <input
       className="text-black"
@@ -246,6 +255,86 @@ export const  ProductNameInput = ({
       value={productName}
       onChange={onChange}
     />
+  );
+};
+
+const ImageContainer = ({ urls }: { urls: string[] }) => {
+  const [selectedImages, setSelectedImages] = useState<string[]>(urls);
+
+  return (
+    <div className="flex flex-wrap">
+      {selectedImages.map((image) => (
+        <div key={image}>
+          <i
+            className="cursor-pointer"
+            onClick={() =>
+              setSelectedImages(
+                selectedImages.filter(
+                  (selectedImage) => selectedImage !== image
+                )
+              )
+            }
+          >
+            X
+          </i>
+          <img src={image} width="300" />
+          <input type="hidden" name="images" value={image} />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+const StoreLinkInput = ({
+  title,
+  name,
+}: {
+  title: string;
+  name: string;
+}) => {
+  const [inputs, setInputs] = useState<{id: string; value: string}[]>([{id: uuidv4(), value: ''}]);
+
+  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>, id: string) => {
+    const newInputs = inputs.map((input) => input.id === id ? {...input, value: e.currentTarget.value} : input )
+    setInputs(newInputs)
+    console.log(newInputs)
+  }
+
+  const newInputRef = useRef<HTMLInputElement | null>(null);
+
+  const  handleAddInput = () => {
+    const newInput = { id: uuidv4(), value: '' };
+    setInputs([...inputs, newInput]);
+
+    // Focus the new input after it's added
+    setTimeout(() => {
+      if (newInputRef.current) {
+        newInputRef.current.focus();
+      }
+    }, 0);
+  };
+  
+  return (
+    <label htmlFor="">
+      {title}
+      {inputs.map((thisInputValue, index) => (
+        <div key={thisInputValue.id}>
+          <input
+            ref={index === inputs.length - 1 ? newInputRef : null}
+            className="text-black"
+            type="text"
+            name={name}
+            value={thisInputValue.value}
+            onChange={(e) =>
+              handleOnChange(e, thisInputValue.id)
+            }
+          />
+        </div>
+      ))}
+      <button onClick={handleAddInput}>
+        add more
+      </button>
+    </label>
   );
 };
 
@@ -260,14 +349,15 @@ export default function NewPrebuiltForm({
 }) {
   const [state, action, pending] = useActionState(submitPrebuilt, undefined);
   const [prebuilt, setPrebuilt] = useState({ processedResults, rawResults });
-
+  const brand = rawResults.brandName;
   return (
     <form key="form" action={action}>
       <h2>Main Info</h2>
-      <ProductNameInput
-        brandName={rawResults.brandName}
-        prebuiltName={rawResults.name}
-      />
+      <input type="hidden" name="brand" value={brand} />
+      <input type="hidden" name="url" value={rawResults.url} />
+      <ProductNameInput brandName={brand} prebuiltName={rawResults.name} />
+      <h2>Images</h2>
+      <ImageContainer urls={rawResults.images} />
       <h2 className="text-xl">Basic Specs</h2>
       <MainSpecsInputs
         processedResults={processedResults}
@@ -286,6 +376,8 @@ export default function NewPrebuiltForm({
           />
         );
       })}
+      <h2>Ecommerce stores</h2>
+      <StoreLinkInput title="Amazon" name="amazon" />
     </form>
   );
 }
