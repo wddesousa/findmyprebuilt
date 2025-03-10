7b499c2e336274af69914e5cc7c83606fb222031
Wilmer Azevedo <wddesousa@gmail.com>
2025-03-10 16:14:43 +0000

    removing env files

--- app/admin/add/prebuilt/actions.ts
+++ app/admin/add/prebuilt/actions.ts
@@ -9,29 +9,29 @@ import { generateSlug } from "@/app/utils";
 import { L } from "vitest/dist/chunks/reporters.nr4dxCkA.js";
 
 const schema = z.object({
-  brand: z.string().nonempty(),
-  name: z.string().min(5).nonempty(),
-  url: z.string().nonempty(),
-  os_id: z.string().nonempty(),
+  brand: z.string().min(1),
+  name: z.string().min(5),
+  url: z.string().min(1),
+  os_id: z.string().min(1),
   base_price: z.coerce.number().min(300).max(20000),
   psu_wattage: z.coerce.number().min(400).max(1400),
   rear_fan_mm: z.coerce.number().min(92).max(140),
   customizable: z.coerce.boolean(),
   front_fan_mm: z.coerce.number().min(92).max(140),
   cpu_cooler_mm: z.coerce.number().min(92).max(140),
-  gpu_chipset_id: z.string().nonempty(),
+  gpu_chipset_id: z.string().min(1),
   memory_modules: z.coerce.number().min(1).max(4),
   cpu_cooler_type: z.nativeEnum(CpuCoolerType),
   main_storage_gb: z.coerce.number().max(24000).min(8),
-  memory_speed_id: z.string().nonempty(),
-  moba_chipset_id: z.string().nonempty(),
+  memory_speed_id: z.string().min(1),
+  moba_chipset_id: z.string().min(1),
   warranty_months: z.coerce.number().min(0).max(60),
   memory_module_gb: z.coerce.number().max(16).min(4),
   secondary_storage_gb: z.coerce.number().max(24000).min(8),
-  main_storage_type_id: z.string().nonempty(),
+  main_storage_type_id: z.string().min(1),
   psu_efficiency_rating: z.nativeEnum(PsuRating),
-  moba_form_factor_id: z.string().nonempty(),
-  case_form_factor: z.string().nonempty(),
+  moba_form_factor_id: z.string().min(1),
+  case_form_factor: z.string().min(1),
   cpu: z.string(),
   gpu: z.string(),
   psu: z.string(),
@@ -44,7 +44,7 @@ const schema = z.object({
   main_storage: z.string(),
   second_storage: z.string(),
   amazon: z.array(z.string()).min(1),
-  images: z.array(z.string().nonempty()).min(1),
+  images: z.array(z.string().min(1)).min(1),
 });
 
 export async function submitPrebuilt(
@@ -73,7 +73,7 @@ export async function submitPrebuilt(
     const image = data.images[index];
     try {
       const uploaded = await uploadImageToCloud(image, generateSlug(data.brand, data.name))
-      data.images[index] = uploaded.public_id;
+      data.images[index] = uploaded.url;
       console.log(uploaded);
     } catch (error) {
       console.error(error);
